8cd73323a707512568ee2072c01d23c148b0e24b
EduArita-coder <eduardoj1109@gmail.com>
2026-07-11 02:03:54 -0600

    cambios realizados en program

--- Program.cs
+++ Program.cs
@@ -8,13 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-// Registrar el DbContext con SQLite
+// Registrar el DbContext con SQLite usando la cadena de conexión de appsettings
 builder.Services.AddDbContext<SimulationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ISimulationService, SimulationService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<SimulationDbContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
