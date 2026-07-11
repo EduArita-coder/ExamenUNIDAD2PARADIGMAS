2be4574533963bdbd3c134c0387a320d62ec3e3a
JhosepSalinas711 <jhosepsal08@gmail.com>
2026-07-10 22:40:15 -0600

    Implementando los Services y agregando los DTOs de proyecciones mensual y anual

---
+++ Dtos/SimulationDtos/MonthlyProjectionDto.cs
@@ -0,0 +1,8 @@
+namespace ExamenUnidad2Paradigmas.Dtos;
+
+public class MonthlyProjectionDto
+{
+    public int Mes { get; set; }
+    public double Balance { get; set; }
+    public double Interes { get; set; }
+}
